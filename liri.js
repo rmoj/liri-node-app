0f29a1b17751147a6864a8a527366cac2bf3d862
rmoj <rmoj99@gmail.com>
2018-08-15 16:48:13 -0400

    Create: function main to process commands

--- liri.js
+++ liri.js
@@ -1,4 +1,36 @@
 require('dotenv').config();
 
+var keys = require('./keys.js');
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+var request = require('request');
+
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+
+function main() {
+  var command = process.argv[2];
+  var item = process.argv[3];
+
+  switch (command) {
+    case 'my-tweets':
+      displayTweets();
+      break;
+
+    case 'spotify-this-song':
+      displaySong(item);
+      break;
+
+    case 'movie-this':
+      displayMovie(item);
+      break;
+
+    case 'do-what-it-says':
+      displayRandomSong();
+      break;
+
+    default:
+      console.log('Unrecognized command.');
+      break;
+  }
+}
